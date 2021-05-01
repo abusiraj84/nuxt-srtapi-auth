d93e1f15a55d9bf4cee661ab3bfee96784e6c702
abusiraj84 <abusiraj84@gmail.com>
2021-05-01 14:48:30 +0300

    fixes

---
+++ layouts/dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="h-screen bg-gray-900 flex">
+    <div
+      class="w-52 bg-gray-800 justify-center items-center h-screen text-white"
+    >
+      <logo class="w-16"></logo>
+      <ul class="flex flex-col items-center space-y-5">
+        <li
+          v-for="item in menu"
+          :key="item.index"
+          class="py-2 px-6 bg-red-400 rounded-xl"
+        >
+          Home
+        </li>
+      </ul>
+    </div>
+    <div>content</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [],
+    }
+  },
+}
+</script>
